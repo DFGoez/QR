c90634b8486ee882cadf9f850961dcab75c4df5f
Diego Goez <dfernandezg@SUMMIT.COM.CO>
2023-02-20 09:21:55 -0500

    20/02/2023

---
+++ CreateQR/CreateQR/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace CreateQR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imgQR = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imgQR)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imgQR
+            // 
+            this.imgQR.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imgQR.Location = new System.Drawing.Point(65, 36);
+            this.imgQR.Name = "imgQR";
+            this.imgQR.Size = new System.Drawing.Size(200, 200);
+            this.imgQR.TabIndex = 0;
+            this.imgQR.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(65, 266);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(65, 306);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Apellido";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(65, 342);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(200, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Cédula";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(65, 381);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(200, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Teléfono";
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(65, 429);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 5;
+            this.btnGenerate.Text = "Generar QR";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Enabled = false;
+            this.btnSave.Location = new System.Drawing.Point(190, 429);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Guardar";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 498);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.imgQR);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imgQR)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox imgQR;
+        private System.Windows.Forms.TextBox label1;
+        private System.Windows.Forms.TextBox label2;
+        private System.Windows.Forms.TextBox label3;
+        private System.Windows.Forms.TextBox label4;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ CreateQR/CreateQR/Form1.cs
@@ -0,0 +1,108 @@
+﻿using Gma.QrCodeNet.Encoding;
+using Gma.QrCodeNet.Encoding.Windows.Render;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text.Json;
+using System.Windows.Forms;
+using JsonSerializer = Newtonsoft.Json.JsonSerializer;
+
+namespace CreateQR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            String datos;
+            QrEncoder qrEncoder = new QrEncoder(ErrorCorrectionLevel.H);
+            QrCode qrCode = new QrCode();
+
+
+            var client = new Client
+            {
+                Name = label1.Text.Trim(),
+                LastName = label2.Text.Trim(),
+                ID = label3.Text.Trim(),
+                Phone = label4.Text.Trim()
+            };
+
+            String jsonString = JsonConvert.SerializeObject(client, Formatting.Indented);
+            //qrEncoder.TryEncode(jsonString, out qrCode);
+
+            string result = string.Empty;
+            byte[] encryted = System.Text.Encoding.Unicode.GetBytes(jsonString);
+            result = Convert.ToBase64String(encryted);
+
+            qrEncoder.TryEncode(result, out qrCode);
+
+            GraphicsRenderer renderer = new GraphicsRenderer(new FixedCodeSize(400, QuietZoneModules.Zero), Brushes.Black, Brushes.White);
+            MemoryStream ms = new MemoryStream();
+            renderer.WriteToStream(qrCode.Matrix, ImageFormat.Png, ms);
+            var imageTemporal = new Bitmap(ms);
+            var imagen = new Bitmap(imageTemporal, new Size(new Point(200, 200)));
+
+            imgQR.BackgroundImage = imagen;
+            imagen.Save("imagen.png", ImageFormat.Png);
+
+            btnSave.Enabled = true;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            Image image = (Image)imgQR.BackgroundImage.Clone();
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.AddExtension = true;
+            saveFileDialog.Filter = "Image PNG (*.png)|*.png";
+            saveFileDialog.ShowDialog();
+            if (!String.IsNullOrEmpty(saveFileDialog.FileName))
+            {
+                image.Save(saveFileDialog.FileName, ImageFormat.Png);
+            }
+            image.Dispose();
+        }
+    }
+}
+
+
+/*
+
+/// Encripta una cadena
+public static string Encriptar(this string _cadenaAencriptar)
+{
+string result = string.Empty;
+byte[] encryted = System.Text.Encoding.Unicode.GetBytes(_cadenaAencriptar);
+result = Convert.ToBase64String(encryted);
+return result;
+}
+
+/// Esta función desencripta la cadena que le envíamos en el parámentro de entrada.
+public static string DesEncriptar(this string _cadenaAdesencriptar)
+{
+string result = string.Empty;
+byte[] decryted = Convert.FromBase64String(_cadenaAdesencriptar);
+//result = System.Text.Encoding.Unicode.GetString(decryted, 0, decryted.ToArray().Length);
+result = System.Text.Encoding.Unicode.GetString(decryted);
+return result;
+}
+}
+En el siguiente código podemos ver cómo usar la clase anterior:
+
+protected void btnInicio_Click(object sender, EventArgs e)
+        {     
+//encriptamos la cadena inicial       
+            txtcadenaencriptada.Text = Seguridad.Encriptar(txtcadenainicial.Text);
+//ahora desencriptamos
+            txtcadenafinal.Text = Seguridad.DesEncriptar(txtcadenaencriptada.Text);
+//Vereis que despues de estas instrucciones txtcadenainial y txtcadenafinal contienen lo mismo.
+
+        }
+
+*/
